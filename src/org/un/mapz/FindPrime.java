85acfdf704743afe671137bb2eaf01396b934ef6
un <66024439+mapzine123@users.noreply.github.com>
2022-03-25 15:39:43 +0900

    소수찾기 sqrt 활용

--- src/org/un/mapz/FindPrime.java
+++ src/org/un/mapz/FindPrime.java
@@ -9,8 +9,8 @@ import java.util.Vector;
 */
 
 public class FindPrime {
-    static int count = 0;
-    
+    static int count1 = 0;
+    static int count2 = 0;
     public static Integer[] getPrimes(int end) {
         Vector<Integer> primes = new Vector<Integer>(); // 소수를 담을 벡터
         for (int i = 2; i <= end; i++) { // 2 ~ 지정된 마지막 숫자까지
@@ -19,7 +19,7 @@ public class FindPrime {
                 if(i % primes.get(idx) == 0) { // 소수로 나누어질 시 소수가 아님
                     isPrime = false;
                 }
-                count++; // 시도 횟수 증가
+                count1++; // 시도 횟수 증가
             }
             if(isPrime) { // 소수라면 벡터에 추가
                 primes.add(i);
@@ -27,15 +27,40 @@ public class FindPrime {
         }
         return primes.toArray(new Integer[0]); // 배열로 반환
     }
-    
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        Integer[] nums = getPrimes(100);
-        long end = System.currentTimeMillis() - start;
-        for (int n : nums) {
-            System.out.println("n = " + n);
+
+    public static int getPrimesPlus(int end) {
+        Vector<Integer> vec = new Vector<Integer>();
+//        vec.add(2);
+        int result = 1;
+        for (int i = 3; i <= end; i++) { // 2 ~ 지정된 마지막 숫자까지
+            boolean isPrime = true;
+            for(int j = 2; isPrime && j <= Math.sqrt(i); j++) {
+                count2++;
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                result++;
+            }
         }
-        System.out.println("count = " + count);
-        System.out.println("end = " + end * 0.001 + "초");
+        return result;
+    }
+
+    public static void main(String[] args) {
+//        long start = System.currentTimeMillis();
+//        Integer[] nums = getPrimes(1000000);
+//        long end1 = System.currentTimeMillis() - start;
+
+        long start2 = System.currentTimeMillis();
+        int nums2 = getPrimesPlus(10000000);
+        long end2 = System.currentTimeMillis() - start2;
+
+//        System.out.println("count1 = " + count1);
+//        System.out.println("end1 = " + end1 * 0.001 + "초");
+
+        System.out.println("count2 = " + count2);
+        System.out.println("end2 = " + end2 * 0.001 + "초");
     }
 }
